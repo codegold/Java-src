9518244a3d15a6bb5ce3b54273d489acc17dce5a
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2018-12-16 20:07:10 +0300

    Added byte type

--- Overload2.java
+++ Overload2.java
@@ -2,12 +2,16 @@
 на выбор перегружаемого метода.
 */
 class Overload2 {
-    void f(int x) {
+    void f(int x) { //f - it is method and int x - his parameter
         System.out.println("Inside f(int): " + x);
     }
 
     void f(double x) {
         System.out.println("Inside f(double): " + x);
+    }
+
+    void f(byte x) {
+        System.out.println("Inside f(byte): " + x);
     }
 }
 
@@ -23,7 +27,7 @@ class TypeConv {
 
         ob.f(i); //call method ob.f(int)
         ob.f(d); //call method ob.f(double)
-        ob.f(b); //call method ob.f(int)  c  преобразованием типов
+        ob.f(b); //call method ob.f(byte)  без  преобразования типов
         ob.f(s); //call method ob.f(int)  c  преобразованием типов
         ob.f(f); //call method ob.f(double)  c  преобразованием типов
     }
