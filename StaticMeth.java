bfddc155e7ae56d9d5801e7796d7742879cedb52
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2018-12-18 13:31:50 +0300

    Usage of static method

---
+++ StaticMeth.java
@@ -0,0 +1,26 @@
+// Применение статического метода,
+class StaticMeth {
+    static int val = 1024; // статическая переменная
+
+    // Статический метод,
+    static int valDiv2() {
+        return val / 2;
+    }
+
+}
+
+class SDemo2 {
+    public static void main(String args[]) {
+
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+
+        StaticMeth.val = 4;
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+    }
+}
+
+
