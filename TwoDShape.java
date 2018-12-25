b96b9bb0e15ff09eb235b9a8daabeed1b144637b
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2018-12-25 12:35:48 +0300

    hierarchy and super method

--- TwoDShape.java
+++ TwoDShape.java
@@ -1,8 +1,25 @@
 // Простая иерархия классов.
 // Класс, описывающий двумерные объекты,»
 class TwoDShape {
-    double height;
-    double width;
+    private double height; //now vars private
+    private double width;
+
+    // Методы доступа к переменным экземпляра width и height.
+    double getWidth() {
+        return width;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    void setWidth(double w) {
+        width = w;
+    }
+
+    void setHeight(double h) {
+        height = h;
+    }
 
     void showDim() {
         System.out.println("height and width is "
@@ -12,46 +29,49 @@ class TwoDShape {
 //Подкласс класса TwoDShape для представления треугольников
 // Класс Triangle наследует от класса TwoDShape»
 
-class Triangle extends  TwoDShape {
+
+class Triangle extends TwoDShape {
     //Из класса Triangle можно обращаться к членам класса
     // TwoDShape таким же обраэом, как и к собственным членам.
-    String style;
+    private String style;
+    //Constructor
+
+    Triangle(String s, double w, double h) {
+        setWidth(w);
+        setHeight(h);
+
+        style = s;
+    }
+
     double area() {
         //Из класса Triangle можно обращаться к членам класса
         // TwoDShape таким же обраэом, как и к собственным членам.
-    return (height * width) / 2;
+        return getHeight() * getWidth() / 2;
     }
+
     void showStyle() {
-        System.out.println("Triangle is " +style);
+        System.out.println("Triangle is " + style);
     }
 }
 
-class Shapes {
+class Shapes3 {
     public static void main(String[] args) {
-        Triangle t1 = new Triangle();
-        Triangle t2 = new Triangle();
+        Triangle t1 = new Triangle("Over painted", 4.0, 4.0);
+        Triangle t2 = new Triangle("Contour", 8.0, 12.0);
 
         // Все члены класса Triangle, даже унаследованные от класса
         // TwoDShape, доступны из объектов типа Triangle.
 
-        t1.style = "isosceles";
-        t1.width = 4;
-        t1.height = 4;
-
-        t2.style = "right";
-        t2.height = 12.6;
-        t2.width = 8.7;
-
-        System.out.println("T1 data is: ");
+        System.out.println("t1 info: ");
         t1.showStyle();
         t1.showDim();
-        System.out.println("T1 area is: " + t1.area());
+        System.out.println("Square - " + t1.area());
         System.out.println();
 
-        System.out.println("T2 data is: ");
+        System.out.println("t2 info: ");
         t2.showStyle();
         t2.showDim();
-        System.out.println("T2 area is: " + t2.area());
+        System.out.println("Square - " + t2.area());
     }
 }
 
