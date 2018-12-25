549ecd625ab706ddcd8eee043515e64c6b0d607a
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2018-12-25 10:08:54 +0300

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+// Подкласс класса TwoDShape, представляющий прямоугольники
+/*class Rectangle extends TwoDShape {
+    boolean isSquare() {
+        if ((getWidth() == getHeight()) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    double area() {
+        return getWidth() * getHeight();
+    }
+}
+*/
