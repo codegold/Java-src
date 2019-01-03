c0b33859f888a9fe716dbd83a96f55690f935c82
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-01-03 13:41:10 +0300

    Errors upgraded with FINAL key word

--- ErrorMsg.java
+++ ErrorMsg.java
@@ -1,12 +1,20 @@
 //Возврат объекта типа String
 class ErrMsg1 {
-    String msgs[] = {
-            "Output error",
-            "Input error",
-            "Out of disc free space",
-            "Out of range limits"
-    };
+    // Коды ошибок.
+    // Константы объявляются с помощью ключевого слова final.
 
+    final int OUTERR = 0;
+    final int INERR = 1;
+    final int DISKERR = 2;
+    final int INDEXERR = 3;
+
+    String msgs[] = {
+    "Output error",
+    "Input error",
+    "Out of disc free space",
+    "Out of range limits"
+    };
+    // Return Error message;
     String getErrMsg1(int i) { // method
         if(i >=0 & i < msgs.length)
             return msgs[i];
@@ -16,11 +24,18 @@ class ErrMsg1 {
     }
 }
 
-class ErrorMsg {
+class FinalD {
     public static void main(String[] args) {
         ErrMsg1 err = new ErrMsg1();
 
-        System.out.println(err.getErrMsg1(2));
+        // При вызове метода используются константы
+        // объявленные с помощью ключевого слова final.
+
+        System.out.println(err.getErrMsg1(err.OUTERR));
+        System.out.println(err.getErrMsg1(err.DISKERR));
+
+        /*System.out.println(err.getErrMsg1(2));
         System.out.println(err.getErrMsg1(72));
+        */
     }
 }
