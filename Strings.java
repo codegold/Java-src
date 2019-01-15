cc04df9224d51201578871a61ac204ee7856078d
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-01-15 15:23:42 +0300

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + x);
+        //Switch s1 = new Variables;
+    }
+}
